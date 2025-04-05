d967807808ac7545b4aa614845fcd5fef7fc5754
tomdchaplin <tomdchaplin@gmail.com>
2025-04-05 09:35:23 -0400

    second

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+
+class CreateUserView(generics.CreateAPIView):#auto create a new user from django
+    queryset = User.objects.all()
+    serializer_class = User #Data to accept to create the user
+    permission_classes = [AllowAny] # Who can create a new user(anyone)
